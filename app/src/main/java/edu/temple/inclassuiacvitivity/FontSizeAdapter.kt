9ff3e7081bfa71e097f5270e099f56c5a53a41d1
Ben-Foran <tuk89220@temple.edu>
2023-02-09 14:30:08 -0500

    start point for FontSizeAdapter

--- app/src/main/java/edu/temple/inclassuiacvitivity/FontSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/FontSizeAdapter.kt
@@ -1,4 +1,29 @@
 package edu.temple.inclassuiacvitivity
 
-class FontSizeAdapter {
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+
+class FontSizeAdapter(_context: Context, _sizes: Array<Int>) :  BaseAdapter() {
+    private val context = _context;
+    private val sizes = _sizes;
+
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(p0: Int): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemId(p0: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        TODO("Not yet implemented")
+    }
+
 }
